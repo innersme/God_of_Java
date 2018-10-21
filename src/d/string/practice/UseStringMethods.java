a61738a184a82aabf8b2d89039ed325cd34938df
MinjaeKim <innersme@gmail.com>
2018-10-21 21:20:45 +0900

    Ch2 Practice Q3

--- src/d/string/practice/UseStringMethods.java
+++ src/d/string/practice/UseStringMethods.java
@@ -2,6 +2,15 @@ package d.string.practice;
 
 public class UseStringMethods {
     public static void main(String[] args){
+        String str = "The String class represents character strings.";
+        UseStringMethods returnStr = new UseStringMethods();
+        returnStr.printWords(str);
+    }
+
+    public void printWords(String str){
+        String[] returnValues = str.split(" ");
+        for (String StringValue : returnValues)
+            System.out.println(StringValue);
 
     }
 }
