481a543c824a4d812c04a2ef440ae07da40b35d8
MinjaeKim <innersme@gmail.com>
2018-10-21 21:22:35 +0900

    Ch2 Practice Q4

--- src/d/string/practice/UseStringMethods.java
+++ src/d/string/practice/UseStringMethods.java
@@ -5,12 +5,17 @@ public class UseStringMethods {
         String str = "The String class represents character strings.";
         UseStringMethods returnStr = new UseStringMethods();
         returnStr.printWords(str);
+        returnStr.findString(str, "string");
     }
 
     public void printWords(String str){
         String[] returnValues = str.split(" ");
         for (String StringValue : returnValues)
             System.out.println(StringValue);
+    }
 
+    public void findString(String str, String findStr){
+        int indexOfStr = str.indexOf(findStr);
+        System.out.println(findStr + " is appeared at " + indexOfStr);
     }
 }
